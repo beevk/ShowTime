aeb270ebb6214d31b3c431661a1a21323115fdac
Bivek Singh <beevk.singh@gmail.com>
2019-06-17 05:45:44 +0545

    Styled intro bg to move along with mouse

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
 
 const Intro = (props) => {
-	return <p className="description">{props.message}</p>;
+	let initialPosition = {
+		x: null,
+		y: null
+	};
+
+	const moveImage = (e) => {
+		e.preventDefault();
+
+		const diff = {
+			x: initialPosition.x - e.nativeEvent.offsetX,
+			y: initialPosition.y - e.nativeEvent.offsetY
+		};
+
+		const elem = e.target;
+		elem.style.backgroundPositionX = diff.x / 2 + 'px';
+		elem.style.backgroundPositionY = diff.y / 2 + 'px';
+	};
+
+	const reset = () => {
+		initialPosition.x = null;
+		initialPosition.y = null;
+	};
+
+	const setInitialValue = (e) => {
+		initialPosition.x = e.nativeEvent.offsetX;
+		initialPosition.y = e.nativeEvent.offsetY;
+	};
+
+	return (
+		<p className="description" onMouseMove={moveImage} onMouseLeave={reset} onMouseEnter={setInitialValue}>
+			{props.message}
+		</p>
+	);
 };
 
 export default Intro;
