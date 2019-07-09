39603b3ffad538bdd747013929822bc99eeda676
Bivek Singh <beevk.singh@gmail.com>
2019-07-09 14:13:59 +0545

    Updates

---
+++ precache-manifest.aac79c753c9c35ba0586733e244fa57e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d665ce90a4856d8573e2",
+    "url": "/ShowTime/static/css/main.e8e2e4c5.chunk.css"
+  },
+  {
+    "revision": "d665ce90a4856d8573e2",
+    "url": "/ShowTime/static/js/main.545753db.chunk.js"
+  },
+  {
+    "revision": "6ea835763b6932de5554",
+    "url": "/ShowTime/static/js/runtime~main.bd53717b.js"
+  },
+  {
+    "revision": "ba591a65ef9f4428f5c9",
+    "url": "/ShowTime/static/js/2.6a1a0f16.chunk.js"
+  },
+  {
+    "revision": "78d0821a87afd11947820ba89846bd5e",
+    "url": "/ShowTime/static/media/loader2.78d0821a.gif"
+  },
+  {
+    "revision": "4529d685f7ad0c5f0d5d5095eae9b238",
+    "url": "/ShowTime/static/media/cover.4529d685.png"
+  },
+  {
+    "revision": "f3b52132874aae1be1708d09a158c7d6",
+    "url": "/ShowTime/index.html"
+  }
+];
